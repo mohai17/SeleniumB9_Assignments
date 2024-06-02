12e83ca9c72ec8d698543c83abc36e53d4535040
mohai <mohai.cse@gmail.com>
2024-06-02 18:00:19 +0600

    Studetn Reg Form 90% done.

--- src/test/java/StudentRegFormTest/StudentRegFormTest.java
+++ src/test/java/StudentRegFormTest/StudentRegFormTest.java
@@ -1,9 +1,11 @@
 package StudentRegFormTest;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
 
@@ -13,8 +15,8 @@ import java.io.File;
 public class StudentRegFormTest {
     public static void main(String[] args) throws InterruptedException {
 
-        WebDriver webDriver = new FirefoxDriver();
-        webDriver.get("https://demoqa.com/automation-practice-form");
+        WebDriver webDriver = new ChromeDriver();
+        webDriver.navigate().to("https://demoqa.com/automation-practice-form");
         webDriver.manage().window().maximize();
         webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
 
@@ -48,8 +50,9 @@ public class StudentRegFormTest {
         webDriver.findElement(By.className("react-datepicker__day--017")).click();
         Thread.sleep(2000);
 
-//        webDriver.findElement(By.id("subjectsInput")).sendKeys("CSE");
-//        Thread.sleep(2000);
+        new Actions(webDriver).moveToElement(webDriver.findElement(By.id("subjectsInput"))).click().sendKeys(Keys.CONTROL).sendKeys("c").perform();
+        new Actions(webDriver).moveToElement(webDriver.findElement(By.id("react-select-2-option-2"))).click().perform();
+        Thread.sleep(2000);
 
         webDriver.findElement(By.cssSelector("label[for='hobbies-checkbox-2']")).click();
         Thread.sleep(2000);
@@ -60,7 +63,14 @@ public class StudentRegFormTest {
 
         webDriver.findElement(By.id("currentAddress")).sendKeys("Rajshahi");
 
-//        webDriver.findElement(By.id("submit")).click();
+        new Actions(webDriver).moveToElement(webDriver.findElement(By.id("react-select-3-input"))).click().perform();
+        new Actions(webDriver).moveToElement(webDriver.findElement(By.id("react-select-3-option-3"))).click().perform();
+        Thread.sleep(3000);
+
+        new Actions(webDriver).moveToElement(webDriver.findElement(By.id("react-select-4-input"))).click().perform();
+        new Actions(webDriver).moveToElement(webDriver.findElement(By.id("react-select-4-option-0"))).click().perform();
+
+        Thread.sleep(3000);
 
         webDriver.quit();
 
